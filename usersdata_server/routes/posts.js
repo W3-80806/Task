bc8e9f0f98edf2dcc551ca5ea01f0d434af67552
W3-80806 <kotgireminal18@gmail.com>
2025-05-29 15:50:51 +0530

    Blog Post API,Database Qerys and Postman Apis Collection Commite

--- usersdata_server/routes/posts.js
+++ usersdata_server/routes/posts.js
@@ -40,4 +40,76 @@ app.get("/", (request, response)=>{
 });
 
 
+//get likes data
+
+
+app.get("/likes", (request, response)=>{
+    var connection = mysql.createConnection(connectionDetails);
+
+    var statement = `select * from likes`;
+
+    connection.query(statement, (error, result)=>{
+        if(error==null)
+        {
+            response.setHeader("Content-Type", "application/json");
+            response.write(JSON.stringify(result));
+            connection.end();
+            response.end();
+        }
+        else
+        {
+            response.setHeader("Content-Type", "application/json");
+             response.write(JSON.stringify(error));
+            connection.end();
+            response.end();
+        }
+    })
+});
+
+// POST /posts/:id/like
+app.post("/:id/like", (request, response) => {
+    var connection = mysql.createConnection(connectionDetails);
+
+    var post_id = request.params.id;
+    var user_id = request.body.user_id;
+    var action = request.body.action; // 'like' or 'dislike'
+
+    var checkStatement = `SELECT * FROM likes WHERE post_id = '${post_id}' AND user_id = '${user_id}'`;
+
+    connection.query(checkStatement, [post_id, user_id], (checkError, checkResult) => {
+        if (checkError) {
+            response.setHeader("Content-Type", "application/json");
+            response.write(JSON.stringify({ error: "Failed to check like", details: checkError }));
+            connection.end();
+            return response.end();
+        }
+
+        var likeExists = checkResult.length > 0;
+
+        var statement;
+        var values;
+
+        if (likeExists) {
+            statement = `UPDATE likes SET action = '${action}' WHERE post_id = '${post_id}' AND user_id = '${user_id}'`;
+            values = [action, postId, user_id];
+        } else {
+            statement = `INSERT INTO likes (post_id, user_id, action) VALUES ('${post_id}','${user_id}','${action}')`;
+            values = [postId, user_id, action];
+        }
+
+        connection.query(statement, values, (error, result) => {
+            response.setHeader("Content-Type", "application/json");
+
+            if (error == null) {
+                response.write(JSON.stringify({ message: `${action} recorded` }));
+            } else {
+                response.write(JSON.stringify({ error: "Failed to update like/dislike", details: error }));
+            }
+
+            connection.end();
+            response.end();
+        });
+    });
+});
+
 module.exports =app;
--- usersdata_server/routes/users.js
+++ usersdata_server/routes/users.js
@@ -68,58 +68,5 @@ app.get("/", (request, response)=>{
     })
 });
 
-// app.put("/:id", (request, response)=>{
-//     var connection = mysql.createConnection(connectionDetails);
-//     var id = request.params.id;
-//     var username = request.body.username;
-//     var password = request.body.password;
-//     var email = request.body.email;
-//     var contactNo = request.body.contactNo;
-
-//     var statement = 
-//         `update usersdata set username='${username}',password='${password}',email='${email}',contactNo='${contactNo}'where id =${id}`;
-
-//     connection.query(statement, (error, result)=>{
-//         if(error==null)
-//         {
-//             response.setHeader("Content-Type", "application/json");
-//             response.write(JSON.stringify(result));
-//             connection.end();
-//             response.end();
-//         }
-//         else
-//         {
-//             response.setHeader("Content-Type", "application/json");
-//             response.write(JSON.stringify(error));
-//             connection.end();
-//             response.end();
-//         }
-//     })
-// });
-// app.delete("/:id", (request, response)=>{
-//     var connection = mysql.createConnection(connectionDetails);
-
-//     var id = request.params.id;//This data belongs to header part 
-  
-//     var statement = 
-//         `delete from usersdata where id =${id}`;
-
-//     connection.query(statement, (error, result)=>{
-//         if(error==null)
-//         {
-//             response.setHeader("Content-Type", "application/json");
-//             response.write(JSON.stringify(result));
-//             connection.end();
-//             response.end();
-//         }
-//         else
-//         {
-//             response.setHeader("Content-Type", "application/json");
-//             response.write(JSON.stringify(error));
-//             connection.end();
-//             response.end();
-//         }
-//     })
-// });
 
 module.exports =app;
